cb8c29f9b3d2955179d839cd9b252cce9363b92e
Sean Mc Ternan <seanmcternan@gmail.com>
2024-08-28 15:02:18 -0400

    replace with env-vars

--- netlify/edge-functions/human-first-party-handler.ts
+++ netlify/edge-functions/human-first-party-handler.ts
@@ -3,14 +3,14 @@ import type { Config, Context } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
     // The requests that sit on the clients side script are proxied to perimiter X and returned from the same domain that requested it thefore circumventeing adblockers
 
-    if (request.url.includes(`/1yz6W67d/init.js`)) {
-        return await fetch(`https://client.perimeterx.net/PX1yz6W67d/main.min.js`, request);
-    } else if (request.url.includes(`/1yz6W67d/captcha/`)) {
+    if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/init.js`)) {
+        return await fetch(`https://client.perimeterx.net/PX${Netlify.env.get("PX_APP_ID")}/main.min.js`, request);
+    } else if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/captcha/`)) {
         const match = request.url.match(/\/1yz6W67d\/captcha\/(.*)/)
-        return await fetch(`https://captcha.px-cdn.net/PX1yz6W67d/captcha.js?${match[1]}`, request);
-    } else if (request.url.includes(`/1yz6W67d/xhr/`)) {
+        return await fetch(`https://captcha.px-cdn.net/PX${Netlify.env.get("PX_APP_ID")}/captcha.js?${match[1]}`, request);
+    } else if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/xhr/`)) {
         const match = request.url.match(/\/1yz6W67d\/xhr\/(.*)/)
-        return await fetch(new Request(`https://collector-PX1yz6W67d.perimeterx.net/${match[1]}`, request));
+        return await fetch(new Request(`https://collector-PX${Netlify.env.get("PX_APP_ID")}.perimeterx.net/${match[1]}`, request));
     } else {
         return new Response("Warning: Edge Function executed, but no action was taken!");
     }
@@ -19,5 +19,5 @@ export default async (request: Request, context: Context) => {
 // The paths defined here are the paths that will be proxied to perimeter X
 export const config: Config = {
     cache: "manual",
-    path: [`/1yz6W67d/init.js`, `/1yz6W67d/captcha/*`, `/1yz6W67d/xhr/*`],
+    path: [`/${Netlify.env.get("PX_APP_ID")}/init.js`, `/${Netlify.env.get("PX_APP_ID")}/captcha/*`, `/${Netlify.env.get("PX_APP_ID")}/xhr/*`],
 };
