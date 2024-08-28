d80d6c0f8beffdc83755f1bfe874e9b6184e9cc3
Sean Mc Ternan <seanmcternan@gmail.com>
2024-08-28 11:32:26 -0400

    add script back

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* In order to use custom parameters,
+          remove the comments and enter the params.
+          <script type="text/javascript">
+            (function(){
+              window._pxParam1 = "<param1>"
+            }());
+          </script> */}
+        {/* In order to by pass add blockers, the script is requested as if it were available on the same route, the Edge Function Collect this requests in and returns the value from same domain, therefore the script is not blocked. */}
+        <script src="/1yz6W67d/init.js" async></script>
       </Head>
       <body>
         <Main />
