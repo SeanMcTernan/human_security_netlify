80c74ba29853dc8614a06196c56681548ce69162
Sean Mc Ternan <seanmcternan@gmail.com>
2024-08-29 09:57:52 -0400

    try removing unneeded paths

--- netlify/edge-functions/human-first-party-handler.ts
+++ netlify/edge-functions/human-first-party-handler.ts
@@ -3,12 +3,7 @@ import type { Config, Context } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
     // The requests that sit on the clients side script are proxied to perimiter X and returned from the same domain that requested it thefore circumventeing adblockers
 
-    if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/init.js`)) {
-        return await fetch(`https://client.perimeterx.net/PX${Netlify.env.get("PX_APP_ID")}/main.min.js`, request);
-    } else if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/captcha/`)) {
-        const match = request.url.match(/\/1yz6W67d\/captcha\/(.*)/)
-        return await fetch(`https://captcha.px-cdn.net/PX${Netlify.env.get("PX_APP_ID")}/captcha.js?${match[1]}`, request);
-    } else if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/xhr/`)) {
+    if (request.url.includes(`/${Netlify.env.get("PX_APP_ID")}/xhr/`)) {
         const match = request.url.match(/\/1yz6W67d\/xhr\/(.*)/)
         return await fetch(new Request(`https://collector-PX${Netlify.env.get("PX_APP_ID")}.perimeterx.net/${match[1]}`, request));
     } else {
@@ -19,5 +14,5 @@ export default async (request: Request, context: Context) => {
 // The paths defined here are the paths that will be proxied to perimeter X
 export const config: Config = {
     cache: "manual",
-    path: [`/${Netlify.env.get("PX_APP_ID")}/init.js`, `/${Netlify.env.get("PX_APP_ID")}/captcha/*`, `/${Netlify.env.get("PX_APP_ID")}/xhr/*`],
+    path: [`/${Netlify.env.get("PX_APP_ID")}/xhr/*`],
 };
